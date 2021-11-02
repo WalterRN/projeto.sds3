47eca3b037b907d9bff97f0277d27f35d074f434
Walter Rutkowski Neto <walter.rutkowskineto@gmail.com>
2021-11-02 17:31:55 -0300

    Basic static componets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NaveBar from "components/NaveBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-      
-    </div>
+    <>
+      <NaveBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
