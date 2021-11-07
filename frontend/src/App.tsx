ccb7a59e36c2bcd400be893bfd5172d275f7a9f7
Walter Rutkowski Neto <walter.rutkowskineto@gmail.com>
2021-11-07 11:25:07 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,8 @@
-import BarChat from "components/BarChat";
-import DataTable from "components/DataTable";
-import DonutChat from "components/DonutChat";
-import Footer from "components/Footer";
-import NaveBar from "components/NaveBar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NaveBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChat/>
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas vendas</h5>
-            <DonutChat/>
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-
-        <DataTable/>
-      </div>
-      <Footer/>
-    </>
+    <Routes />
   );
 }
 
